f0976a441183fd0cb757ae59c42a6edcc42bacc0
Ирина <Appricotpie@yandex.ru>
2014-11-10 09:33:26 +0300

    Initial commit

---
+++ src/sample/TestReader.java
@@ -0,0 +1,27 @@
+package sample;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestReader {
+    public TestReader(String filePath) {
+        this.filePath = filePath;
+    }
+
+    String filePath;
+
+    public List<Double> read() throws IOException {
+        List<Double> list = new ArrayList<>();
+        FileReader fr = new FileReader(filePath);
+        BufferedReader bufferedReader = new BufferedReader(fr);
+        String s;
+        while ((s = bufferedReader.readLine()) != null) {
+            Double i = Double.valueOf(s);
+            list.add(i);
+        }
+        return list;
+    }
+}
