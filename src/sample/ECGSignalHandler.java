5f3b483a914a4cd8e66d1591b757433e6297bd6a
Irina Susanu <Appricotpie@yandex.ru>
2014-12-12 22:47:42 +0300

    Изменены методы производной, возведения в квадрат. Найден первый пик, добавлены другие методы по нахождению порогов и остальных пиков

--- src/sample/ECGSignalHandler.java
+++ src/sample/ECGSignalHandler.java
@@ -12,50 +12,99 @@ public class ECGSignalHandler extends Thread {
     private List<Double> signal = new ArrayList<>();
     private List<Double> squaring = new ArrayList<>();
     private List<Double> movingWindowIntegration = new ArrayList<>();
-    private List<Double> Peaki = new ArrayList<>();
-    private List<Double> Peakifinal = new ArrayList<>();
-    private boolean isRun;
+    private List<Double> peaki = new ArrayList<>();
+    private List<Double> peakifinal = new ArrayList<>();
+    private boolean firstPeakFound = false;
+    private final int integrationWindowWidth = 50;
+    private int lastPeakIndex = 0;
 
     public ECGSignalHandler(BlockingQueue queue) {
         this.queue = queue;
+        for (int i = 0; i < 5; i++)
+            signal.add(0.0);
+        for (int i = 0; i < integrationWindowWidth; i++)
+            squaring.add(0.0);
+        movingWindowIntegration.add(0.0);
     }
 
-    public void handle() throws InterruptedException {
-        while (isRun) {
+    public void run() {
+        while (true) {
             if (!queue.isEmpty()) {
-                Double newD = queue.take();
+                Double newD = null;
+                try {
+                    newD = queue.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 signal.add(newD);
             }
-            setDerivative();
-            setSquaring();
-            for (int i = 0; i < signal.size(); i++) {
-                System.out.println(signal.get(i));
+            updateDerivative();
+            updateSquaring();
+            updateMovingWindowIntegration();
+            if (!firstPeakFound) {
+                findFirstPeak();
+            } else {
+                //todo приватный метод определения порогов "открытия" окна интегрирования
+                if (thresholdIsOpen()) {
+                    findRRPeaks();
+                }
             }
         }
     }
 
-    public void setRun(boolean isRun) {
-        this.isRun = isRun;
+    private boolean thresholdIsOpen() {
+        return false;  //если текущее значение >= порога, то return true
     }
 
-    private void setDerivative() {
-        //цикл, находим сглаженную производную несглаженного сигнала
-        for (int i = 2; i < signal.size() - 2; i++) {
-            derivative.add(0.1 * (2 * signal.get(i - 2)) - signal.get(i - 1) +
-                    signal.get(i + 1) + 2 * signal.get(i + 2) / 0.002);
+    private void integrationWindowLeftThreshold() {
+        //todo 10% от последнего пика
+    }
+
+    private void updateDerivative() {
+        int n = signal.size() - 1;
+        // 1/8 = 0
+        //1.0/8.0 = 0.125
+
+        derivative.add(1.0 / 8.0 * (2 * (signal.get(n)) + signal.get(n - 1) - signal.get(n - 3) - 2 * signal.get(n - 4)) / 0.002);
+        //System.out.println(derivative.get(derivative.size()-1));
+    }
+
+    private void updateSquaring() {
+        squaring.add(pow(derivative.get(derivative.size() - 1), 2));
+        //System.out.println(squaring.get(squaring.size()-1));
+    }
+
+    private void updateMovingWindowIntegration() {
+        int n = squaring.size() - 1;
+        Double sum = 0.0;
+        for (int i = n; i >= n - (integrationWindowWidth - 1); i--) {
+            sum = sum + squaring.get(i); //sum += squaring.get(i);
         }
+        sum /= integrationWindowWidth;
+        movingWindowIntegration.add(sum);
+        //System.out.println(sum);
 
-//        for (int i = 0; i < listFirst.size(); i++)
-        //squaring.add(signal.get(signal.size() - 1));
     }
 
-    private void setSquaring() {
-        for (int i = 0; i < (derivative.size() - 1); i++) {
-            squaring.add(pow(derivative.get(i), 2));
+    private void findFirstPeak() {
+        if (movingWindowIntegration.get(movingWindowIntegration.size() - 1) < (movingWindowIntegration.get(movingWindowIntegration.size() - 2))) {
+            lastPeakIndex = movingWindowIntegration.size() - 2;
+            firstPeakFound = true;
         }
+        //вычисление производных
+        //сравниваешь производную с нулем
+        //если производнаяменьше нуля, выходим из цикла
+        //и добавляем соответствующий (максимум функции интегрирования минус ширина окна) элемент в массив пиков
+        //}
+
     }
 
-    private void setMovingWindowIntegration() {
+    private void findRRPeaks() {
+        //todo искать максимум функции и обновлять lastpeakindex
+        integrationWindowLeftThreshold();//если нашли пик, 10% от нового пика
+    }
+
+    private void setMovingWindowIntegration1() {
         double tQRS = 0.1;
         double Td = 0.002;
         int W = 50;
@@ -64,17 +113,17 @@ public class ECGSignalHandler extends Thread {
             movingWindowIntegration.add(1 / W * (squaring.get(i - (W - i))));
             double max = 0;
             if (movingWindowIntegration.get(i) > max) max = movingWindowIntegration.get(i);
-            Peaki.add(max);
+            peaki.add(max);
         }
         Double SPKI = 0.0;
-        SPKI = 0.125 * Peaki.get(1) + 0.875 * SPKI;
+        SPKI = 0.125 * peaki.get(1) + 0.875 * SPKI;
         Double NPKI = 0.0;
-        NPKI = 0.125 * Peaki.get(1) + 0.875 * NPKI;
+        NPKI = 0.125 * peaki.get(1) + 0.875 * NPKI;
         Double ThresholdI1 = NPKI + 0.25 * (SPKI - NPKI);
         Double ThresholdI2 = 0.5 * ThresholdI1;
-        for (int i = 0; i < Peaki.size(); i++) {
-            if (Peaki.get(i) > ThresholdI1) {
-                Peakifinal.add(Peaki.get(i));
+        for (int i = 0; i < peaki.size(); i++) {
+            if (peaki.get(i) > ThresholdI1) {
+                peakifinal.add(peaki.get(i));
             } else {
 
             }
@@ -85,10 +134,6 @@ public class ECGSignalHandler extends Thread {
                 sum = 1/W*(squaring.get(n - (W-i)))+sum;
             }
         }*/
-    }
-
-    @Override
-    public void run() {
     }
 
 }
